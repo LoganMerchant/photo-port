37e36b499cb797e6670f6f35639232a54c754e9f
Logan Merchant <merchantclogan@gmail.com>
2020-12-14 18:10:34 -0700

    add initial nav setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import './App.css';
-import About from './components/About/index';
+import React from "react";
+import "./App.css";
+import About from "./components/About/index";
+import Nav from "./components/Nav/index";
 
 function App() {
   return (
     <div>
+        <Nav />
       <main>
         <About />
       </main>
