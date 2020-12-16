5d5e100d74e08bdf9ab0292e6c6a11d089f3736b
Logan Merchant <merchantclogan@gmail.com>
2020-12-15 20:56:31 -0700

    add hardcode to display photo in gallery

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import About from "./components/About/index";
 import Nav from "./components/Nav/index";
+import Gallery from "./components/Gallery/index";
 
 function App() {
   return (
     <div>
-        <Nav />
+      <Nav />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
